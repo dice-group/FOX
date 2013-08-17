18e08f22c2f10791cde77c085cd3df942afe5d01
renespeck <speckrene@gmail.com>
2013-08-17 18:30:39 +0200

    Set default value of foxlight to 1.

--- demo/js/controller.js
+++ demo/js/controller.js
@@ -24,6 +24,7 @@ Fox.DemoCtrl = function($routeParams, $scope,$http) {
            $scope.foxRequest.input="http://en.wikipedia.org/wiki/Leipzig_University";
            $scope.foxRequest.output='RDF/XML';
            $scope.foxRequest.nif=0;
+           $scope.foxRequest.foxlight=1;
            
        }else if(newValue == 1){
            $scope.foxRequest.type ='text';
@@ -31,6 +32,7 @@ Fox.DemoCtrl = function($routeParams, $scope,$http) {
            $scope.foxRequest.input="Cologne German: Köln, Kölsch: Kölle is Germany's fourth-largest city (after Berlin, Hamburg, and Munich), and is the largest city both in the German Federal State of North Rhine-Westphalia and within the Rhine-Ruhr Metropolitan Area, one of the major European metropolitan areas with more than ten million inhabitants. Cologne is located on both sides of the Rhine River. The city's famous Cologne Cathedral (Kölner Dom) is the seat of the Catholic Archbishop of Cologne. The University of Cologne (Universität zu Köln) is one of Europe's oldest and largest universities.";
            $scope.foxRequest.output='TURTLE';
            $scope.foxRequest.nif=1;
+           $scope.foxRequest.foxlight=1;
            
        }else{ // default 0 or not def.
            $scope.foxRequest.type ='text';
@@ -38,6 +40,7 @@ Fox.DemoCtrl = function($routeParams, $scope,$http) {
            $scope.foxRequest.input=("The foundation of the University of Leipzig in 1409 initiated the city's development into a centre of German law and the publishing industry, and towards being a location of the Reichsgericht (High Court), and the German National Library (founded in 1912). The philosopher and mathematician Gottfried Wilhelm Leibniz was born in Leipzig in 1646, and attended the university from 1661-1666.");
            $scope.foxRequest.output='JSONLD';
            $scope.foxRequest.nif=0;
+           $scope.foxRequest.foxlight=1;
        }
     });
     
