935bbb5885ed70472bfa1e926e6c66ecebbd85b5
renespeck <speckrene@gmail.com>
2013-09-01 22:43:49 +0200

    Update output.

--- src/main/java/org/aksw/fox/CrossValidation.java
+++ src/main/java/org/aksw/fox/CrossValidation.java
@@ -1,7 +1,5 @@
 package org.aksw.fox;
 
-import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Random;
@@ -33,7 +31,28 @@ public class CrossValidation {
 
     public static FoxNERTools foxNERTools = new FoxNERTools();
     static int seed = 1, folds = 10;
-    static Map<Integer, Map<String, List<Double>>> kMap = new HashMap<>();
+
+    public static void myprint(Evaluation eval, Classifier classifier, Instances instances) {
+
+        logger.info("=== Run information ===\n\n");
+        logger.info("Scheme: " + classifier.getClass().getName() + " Options: " + Utils.joinOptions(classifier.getOptions()));
+        logger.info("Relation: " + instances.relationName());
+        logger.info("Instances: " + instances.numInstances());
+        logger.info("Attributes: " + instances.numAttributes());
+
+        logger.info("=== Classifier model ===\n\n");
+        logger.info(classifier.toString());
+
+        logger.info("=== Summary ===\n");
+        logger.info(eval.toSummaryString());
+
+        try {
+            logger.info(eval.toClassDetailsString());
+            logger.info(eval.toMatrixString());
+        } catch (Exception e) {
+            logger.error("\n", e);
+        }
+    }
 
     public static void crossValidation(Classifier cls, String[] inputFiles) throws Exception {
 
@@ -82,12 +101,11 @@ public class CrossValidation {
             Classifier clsCopy = Classifier.makeCopy(cls);
             clsCopy.buildClassifier(train);
             eval.evaluateModel(clsCopy, test);
-
             printConfusionMatrix(eval);
             printMeasures(eval);
         }
-        printSetup(cls, eval, randInstances);
-        printOverall();
+
+        myprint(eval, cls, randInstances);
     }
 
     public static void printConfusionMatrix(Evaluation eval) {
@@ -107,11 +125,6 @@ public class CrossValidation {
 
     public static void printMeasures(Evaluation eval) {
 
-        // remember values
-        int currentK = kMap.keySet().size() + 1;
-        kMap.put(currentK, new HashMap<String, List<Double>>());
-
-        //
         final List<String> cat = EntityClassMap.entityClasses;
         for (String cl : EntityClassMap.entityClasses) {
 
@@ -124,61 +137,6 @@ public class CrossValidation {
             logger.info("fMeasure: " + f1);
             logger.info("precision: " + p);
             logger.info("recall: " + r);
-
-            kMap.get(currentK).put(cl, new ArrayList<Double>());
-            kMap.get(currentK).get(cl).add(f1);
-            kMap.get(currentK).get(cl).add(p);
-            kMap.get(currentK).get(cl).add(r);
         }
-    }
-
-    public static void printSetup(Classifier cls, Evaluation eval, Instances instances) {
-        logger.info("=== setup ===");
-        logger.info("Classifier: " + cls.getClass().getName() + " " + Utils.joinOptions(cls.getOptions()));
-        logger.info("Dataset: " + instances.relationName());
-        logger.info(eval.toSummaryString("=== " + folds + "-fold Cross-validation ===", false));
-    }
-
-    public static void printOverall() {
-
-        logger.info("=== total ===");
-
-        int k = kMap.size();
-
-        Map<String, List<Double>> classTotal = new HashMap<>();
-        for (int i = 1; i <= k; i++) {
-
-            Map<String, List<Double>> classValues = kMap.get(i);
-            for (String cls : classValues.keySet()) {
-
-                // init.
-                if (classTotal.get(cls) == null) {
-                    classTotal.put(cls, new ArrayList<Double>());
-
-                    classTotal.get(cls).add(0D);
-                    classTotal.get(cls).add(0D);
-                    classTotal.get(cls).add(0D);
-                }
-                classTotal.get(cls).set(0, classTotal.get(cls).get(0) + classValues.get(cls).get(0));
-                classTotal.get(cls).set(1, classTotal.get(cls).get(1) + classValues.get(cls).get(1));
-                classTotal.get(cls).set(2, classTotal.get(cls).get(2) + classValues.get(cls).get(2));
-            }
-        }
-        double f1 = 0, p = 0, r = 0;
-        for (String cls : classTotal.keySet()) {
-
-            f1 += classTotal.get(cls).get(0) / k;
-            p += classTotal.get(cls).get(1) / k;
-            r += classTotal.get(cls).get(2) / k;
-
-            logger.info("f1 " + cls + ": " + classTotal.get(cls).get(0) / k);
-            logger.info("p " + cls + ": " + classTotal.get(cls).get(1) / k);
-            logger.info("r " + cls + ": " + classTotal.get(cls).get(2) / k);
-        }
-        logger.info("=== overall ===");
-        logger.info("f1: " + f1 / classTotal.keySet().size());
-        logger.info("p: " + p / classTotal.keySet().size());
-        logger.info("r: " + r / classTotal.keySet().size());
-
     }
 }
