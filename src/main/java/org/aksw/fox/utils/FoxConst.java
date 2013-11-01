d38d83dbf095fd424d30267c3a9318e17a94f46d
renespeck <speckrene@gmail.com>
2013-11-01 20:56:28 +0100

    Examples.

---
+++ src/main/java/org/aksw/fox/utils/FoxConst.java
@@ -0,0 +1,18 @@
+package org.aksw.fox.utils;
+
+public class FoxConst {
+
+    public static String EXAMPLE_1 = "The foundation of the University of Leipzig in 1409 initiated the city's development into a " +
+            "centre of German law and the publishing industry, and towards being a location of the " +
+            "Reichsgericht (High Court), and the German National Library (founded in 1912)." +
+            " The philosopher and mathematician Gottfried Wilhelm Leibniz was born in Leipzig in 1646," +
+            " and attended the university from 1661-1666.";
+
+    public static String EXAMPLE_2 = "Cologne German: Köln, Kölsch: Kölle is Germany's fourth-largest city (after Berlin, Hamburg, and Munich), " +
+            "and is the largest city both in the German Federal State of North Rhine-Westphalia and within the Rhine-Ruhr Metropolitan Area, " +
+            "one of the major European metropolitan areas with more than ten million inhabitants. " +
+            "Cologne is located on both sides of the Rhine River. " +
+            "The city's famous Cologne Cathedral (Kölner Dom) is the seat of the Catholic Archbishop of Cologne. " +
+            "The University of Cologne (Universität zu Köln) is one of Europe's oldest and largest universities.";
+
+}
