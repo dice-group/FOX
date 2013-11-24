8ab97ad09b2825a1e0bcb254c690687e4d25dcb7
renespeck <speckrene@gmail.com>
2013-11-24 21:28:54 +0100

    Downloads dependencies.

--- fox_build.sh
+++ fox_build.sh
@@ -1,2 +1,14 @@
-mvn clean install javadoc:javadoc
+#!/bin/bash
 
+file="lib/stanford-corenlp-3.2.0-models.jar"
+url="http://repo1.maven.org/maven2/edu/stanford/nlp/stanford-corenlp/3.2.0/stanford-corenlp-3.2.0-models.jar"
+
+if [ ! -f "$file" ]; then
+    echo "Downloading dependencies ... ($url)"
+    curl --retry 4 -o "$file" "$url"
+fi
+if [ -f "$file" ]; then
+    mvn clean install javadoc:javadoc
+else
+    echo "Couldn't download dependency: $url"
+fi
