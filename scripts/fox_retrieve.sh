d1c5b1841aeb4416d63f2d1b5a55a9d54d60c5e0
renespeck <speckrene@gmail.com>
2014-01-22 23:48:22 +0100

    Update version.

--- scripts/fox_retrieve.sh
+++ scripts/fox_retrieve.sh
@@ -1,4 +1,1 @@
-nohup java -Xmx5G -cp fox-2.0.0-jar-with-dependencies.jar org.aksw.fox.MainFox -aretrieve -iinput/1 > fox_retrieve.log &
-
-
-
+nohup java -Xmx5G -cp fox-2.1.0-jar-with-dependencies.jar org.aksw.fox.MainFox -aretrieve -iinput/1 > fox_retrieve.log &
--- scripts/fox_train.sh
+++
@@ -1,4 +0,0 @@
-nohup java -Xmx5G -cp fox-2.0.0-jar-with-dependencies.jar org.aksw.fox.MainFox -atrain -iinput/1 > fox_train.log &
-
-
-
