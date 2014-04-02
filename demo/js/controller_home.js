0b188a27cffdd4979b431ff90180694325e739e6
renespeck <speckrene@gmail.com>
2014-04-02 13:19:16 +0200

    Updates demo.

---
+++ demo/js/controller_home.js
@@ -0,0 +1,7 @@
+'use strict';
+
+Fox.HomeCtrl = function($routeParams, $scope) {
+    $scope.currentCtrl = 'HomeCtrl';
+};
+
+Fox.controller('HomeCtrl', Fox.HomeCtrl);
--- demo/js/factory.js
+++ demo/js/factory.js
@@ -1,10 +1,11 @@
 Fox.factory('JsonService', function($resource) {
 	return $resource(
-			':file',
+			'examples/:file',
 			{ }, 
 			{
 				feedbackExp: {method:'GET', isArray: false, params: { file: 'feedbackExp.json' } },
 				nerExp: {method:'GET', isArray: false, params: { file: 'nerExp.json' } },
+                responseExp: {method:'GET', isArray: false, params: { file: 'response.json' } }
 			}
 	);
 });
