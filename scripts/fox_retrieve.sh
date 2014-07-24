63f3b7b3734bf1374ef66da1f9ec250b5cea8432
renespeck <speckrene@gmail.com>
2014-07-24 14:36:21 +0200

    Updates version to 2.2.1.

--- scripts/fox_retrieve.sh
+++ scripts/fox_retrieve.sh
@@ -1,1 +1,1 @@
-nohup java -Xmx5G -cp fox-2.2.0-jar-with-dependencies.jar org.aksw.fox.MainFox -aretrieve -iinput/1 > fox_retrieve.log &
+nohup java -Xmx5G -cp fox-2.2.1-jar-with-dependencies.jar org.aksw.fox.MainFox -aretrieve -iinput/1 > fox_retrieve.log &
