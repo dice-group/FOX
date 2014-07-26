1be2d51b089fdc16275ffa22212527c016b2cabe
renespeck <speckrene@gmail.com>
2014-07-26 15:02:31 +0200

    Update version and minor changes.

--- fox_build.sh
+++ fox_build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-
-echo "Building FOX..."
+#
 # check installed dependencies
+echo "Checking dependencies..."
 file="lib/stanford-corenlp-3.2.0-models.jar"
 url="http://repo1.maven.org/maven2/edu/stanford/nlp/stanford-corenlp/3.2.0/stanford-corenlp-3.2.0-models.jar"
 if [ ! -f "$file" ]; then
@@ -11,7 +11,7 @@ if [ ! -f "$file" ]; then
         echo "Couldn't downloading dependency: $file"
     fi
 fi
-
+#
 # check installed dependencies
 pkg="graphviz"
 if dpkg --get-selections | grep -q "^$pkg[[:space:]]*install$" >/dev/null; then
@@ -20,16 +20,13 @@ else
     echo "Please install: $pkg"
     echo "(sudo apt-get install $pkg)"
     echo
-    #if apt-get install $pkg; then
-    #    echo "Successfully installed $pkg"
-    #else
-    #    echo "Error installing $pkg"
-    #fi
 fi
-
+#
 # build fox
+echo "Building FOX..."
 if [ -f "$file" ] & dpkg --get-selections | grep -q "^$pkg[[:space:]]*install$" >/dev/null; then
     mvn clean install javadoc:javadoc
 else
     echo "Couldn't build FOX."
 fi
+#
--- scripts/fox_train.sh
+++ scripts/fox_train.sh
@@ -1,1 +1,1 @@
-nohup java -Xmx5G -cp fox-2.2.1-jar-with-dependencies.jar org.aksw.fox.MainFox -atrain -iinput/1 > fox_train.log &
+nohup java -Xmx5G -cp fox-2.2.2-jar-with-dependencies.jar org.aksw.fox.MainFox -atrain -iinput/1 > fox_train.log &
