4cfb880f2a354c95ed64a2b6f9be6e8028b7b6a3
renespeck <speckrene@gmail.com>
2014-08-05 15:15:15 +0200

    Change port.

--- scripts/fox_run.sh
+++ scripts/fox_run.sh
@@ -1,1 +1,1 @@
-nohup java -Xmx10G -cp fox-2.2.2-jar-with-dependencies.jar org.aksw.fox.MainServer -p4443 > fox.log &
+nohup java -Xmx10G -cp fox-2.2.2-jar-with-dependencies.jar org.aksw.fox.MainServer -p4444 > fox.log &
