4e5a105078a5b2a66bba499ac495f5f2bb24b6ae
renespeck <speckrene@gmail.com>
2015-01-05 13:16:38 +0100

    Readme and build update.

--- build.sh
+++ build.sh
@@ -2,15 +2,17 @@
 #
 # check installed dependencies
 echo "Checking dependencies..."
-file="lib/stanford-corenlp-3.2.0-models.jar"
-url="http://repo1.maven.org/maven2/edu/stanford/nlp/stanford-corenlp/3.2.0/stanford-corenlp-3.2.0-models.jar"
-if [ ! -f "$file" ]; then
-    echo "Downloading dependencies ... ($url)"
-    curl --retry 4 -o "$file" "$url"
-    if [ ! -f "$file" ]; then
-        echo "Couldn't downloading dependency: $file"
-    fi
-fi
+
+#file="lib/stanford-corenlp-3.2.0-models.jar"
+#url="http://repo1.maven.org/maven2/edu/stanford/nlp/stanford-corenlp/3.2.0/stanford-corenlp-3.2.0-models.jar"
+#if [ ! -f "$file" ]; then
+#    echo "Downloading dependencies ... ($url)"
+#    curl --retry 4 -o "$file" "$url"
+#    if [ ! -f "$file" ]; then
+#        echo "Couldn't downloading dependency: $file"
+#    fi
+#fi
+
 #
 # check installed dependencies
 pkg="graphviz"
