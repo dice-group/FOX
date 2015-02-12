731c8f4f15e44996d346484f13ff2b406cd91a6e
renespeck <speckrene@gmail.com>
2015-02-12 10:04:38 +0100

    Change default example.

--- demo/js/controller_demo.js
+++ demo/js/controller_demo.js
@@ -21,6 +21,7 @@ Fox.DemoCtrl = function($routeParams, $scope, $http) {
             $scope.foxRequest.type = 'text';
             $scope.foxRequest.input = "Berlin is an American New Wave band. Despite its name, Berlin did not have any known major connections with Germany, but instead was formed in Los Angeles, California in 1978.";
             $scope.foxRequest.output = 'Turtle';
+
         } else if (newValue == 2) {
             $scope.foxRequest.type = 'url';
             $scope.foxRequest.input = "http://en.wikipedia.org/wiki/Leipzig_University";
@@ -31,13 +32,17 @@ Fox.DemoCtrl = function($routeParams, $scope, $http) {
             $scope.foxRequest.input = "Cologne German: Köln, Kölsch: Kölle is Germany's fourth-largest city (after Berlin, Hamburg, and Munich), and is the largest city both in the German Federal State of North Rhine-Westphalia and within the Rhine-Ruhr Metropolitan Area, one of the major European metropolitan areas with more than ten million inhabitants. Cologne is located on both sides of the Rhine River. The city's famous Cologne Cathedral (Kölner Dom) is the seat of the Catholic Archbishop of Cologne. The University of Cologne (Universität zu Köln) is one of Europe's oldest and largest universities.";
             $scope.foxRequest.output = 'Turtle';
 
+        } else if (newValue == 4) {
+            $scope.foxRequest.type = 'text';
+            $scope.foxRequest.input = "The foundation of the University of Leipzig in 1409 initiated the city's development into a centre of German law and the publishing industry, and towards being a location of the Reichsgericht (High Court), and the German National Library (founded in 1912). The philosopher and mathematician Gottfried Wilhelm Leibniz was born in Leipzig in 1646, and attended the university from 1661-1666.";
+            $scope.foxRequest.output = 'JSON-LD';
+
         } else { // default 0 or not def.
             $scope.foxRequest.type = 'text';
-            $scope.foxRequest.input = ("The foundation of the University of Leipzig in 1409 initiated the city's development into a centre of German law and the publishing industry, and towards being a location of the Reichsgericht (High Court), and the German National Library (founded in 1912). The philosopher and mathematician Gottfried Wilhelm Leibniz was born in Leipzig in 1646, and attended the university from 1661-1666.");
+            $scope.foxRequest.input = "The philosopher and mathematician Leibniz was born in Leipzig in 1646 and attended the University of Leipzig from 1661-1666. The current chancellor of Germany, Angela Merkel, also attended this university. ";
             $scope.foxRequest.output = 'JSON-LD';
         }
     });
-
 
     $scope.foxResponse = {
         input: '',
