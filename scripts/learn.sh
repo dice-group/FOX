3124ba8bcd201a393c209b41cd8db19438b64b0b
renespeck <speckrene@gmail.com>
2015-02-15 22:58:03 +0100

    Update scripts version.

--- scripts/learn.sh
+++ scripts/learn.sh
@@ -1,1 +1,1 @@
-nohup java -Xmx5G -cp fox-2.2.2-jar-with-dependencies.jar org.aksw.fox.MainFox -atrain -iinput/1 > learn.log &
+nohup java -Xmx5G -cp fox-2.3.0-jar-with-dependencies.jar org.aksw.fox.MainFox -atrain -iinput/1 > learn.log &
