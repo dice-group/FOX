ea71760108f2c90bcd1357e81f5a313d0a1d47fc
renespeck <speckrene@gmail.com>
2015-02-25 18:03:48 +0100

    Updates build script.

--- build.sh
+++ build.sh
@@ -3,18 +3,6 @@
 # check installed dependencies
 echo "Checking dependencies..."
 
-#file="lib/stanford-corenlp-3.2.0-models.jar"
-#url="http://repo1.maven.org/maven2/edu/stanford/nlp/stanford-corenlp/3.2.0/stanford-corenlp-3.2.0-models.jar"
-#if [ ! -f "$file" ]; then
-#    echo "Downloading dependencies ... ($url)"
-#    curl --retry 4 -o "$file" "$url"
-#    if [ ! -f "$file" ]; then
-#        echo "Couldn't downloading dependency: $file"
-#    fi
-#fi
-
-#
-# check installed dependencies
 pkg="graphviz"
 if dpkg --get-selections | grep -q "^$pkg[[:space:]]*install$" >/dev/null; then
      echo "$pkg is already installed"
@@ -27,7 +15,7 @@ fi
 # build fox
 echo "Building FOX..."
 if [ -f "$file" ] & dpkg --get-selections | grep -q "^$pkg[[:space:]]*install$" >/dev/null; then
-    mvn clean install javadoc:javadoc
+    mvn clean package javadoc:javadoc
 else
     echo "Couldn't build FOX."
 fi
