a13427b2ac0f6a54db8959cd68f38a94536a04e6
renespeck <speckrene@gmail.com>
2015-03-02 12:11:28 +0100

    Fix bug.

--- src/main/java/org/aksw/fox/utils/FoxTextUtil.java
+++ src/main/java/org/aksw/fox/utils/FoxTextUtil.java
@@ -103,7 +103,8 @@ public class FoxTextUtil {
         // TODO:add all possible cases
 
         Source src = new Source(html);
-        return new TextExtractor(new Segment(src, src.getBegin(), src.getEnd())).setConvertNonBreakingSpaces(false).toString();
+        return new TextExtractor(new Segment(src, src.getBegin(), src.getEnd()))
+                .setConvertNonBreakingSpaces(true).toString();
     }
 
     /**
@@ -241,7 +242,7 @@ public class FoxTextUtil {
         if (token != null && tokenInput != null && token.length() < tokenInput.length()) {
 
             token = new StringBuilder().append(" ").append(token.trim()).append(" ").toString();
-            tokenInput = new StringBuilder().append(" ").append(tokenInput.trim()).append(" ").toString();
+            tokenInput = new StringBuilder().append(" ").append(tokenInput).append(" ").toString();
 
             token = Pattern.quote(token);
             Matcher matcher = Pattern.compile(token).matcher(tokenInput);
