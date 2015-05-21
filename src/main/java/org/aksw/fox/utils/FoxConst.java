d25dbe63ae9c0a07dd9c4b2ea2cd32fd10845a2a
Randall Graves <alexander.pirogov@yahoo.com>
2015-05-21 14:23:40 +0200

    FoxConst - Spanish example (minor changes)

--- src/main/java/org/aksw/fox/utils/FoxConst.java
+++ src/main/java/org/aksw/fox/utils/FoxConst.java
@@ -15,19 +15,22 @@ public class FoxConst {
                                                                     " The philosopher and mathematician Gottfried Wilhelm Leibniz was born in Leipzig in 1646," +
                                                                     " and attended the university from 1661-1666.";
 
-    public static String       NER_EN_EXAMPLE_2             = "Cologne German: Köln, Kölsch: Kölle is Germany's fourth-largest city (after Berlin, Hamburg, and Munich), " +
+    public static String       NER_EN_EXAMPLE_2             = "Cologne German: KÃ¶ln, KÃ¶lsch: KÃ¶lle is Germany's fourth-largest city (after Berlin, Hamburg, and Munich), " +
                                                                     "and is the largest city both in the German Federal State of North Rhine-Westphalia and within the Rhine-Ruhr Metropolitan Area, " +
                                                                     "one of the major European metropolitan areas with more than ten million inhabitants. " +
                                                                     "Cologne is located on both sides of the Rhine River. " +
-                                                                    "The city's famous Cologne Cathedral (Kölner Dom) is the seat of the Catholic Archbishop of Cologne. " +
-                                                                    "The University of Cologne (Universität zu Köln) is one of Europe's oldest and largest universities.";
+                                                                    "The city's famous Cologne Cathedral (KÃ¶lner Dom) is the seat of the Catholic Archbishop of Cologne. " +
+                                                                    "The University of Cologne (UniversitÃ¤t zu KÃ¶ln) is one of Europe's oldest and largest universities.";
 
     public static String       NER_EN_EXAMPLE_3             = "Berlin is an American New Wave band. Despite its name, Berlin did not have any known major connections " +
                                                                     "with Germany, but instead was formed in Los Angeles, California in 1978.";
 
-    public static String       NER_GER_EXAMPLE_1            = "Nachdem infolge von Streitigkeiten an der (Karls-Universität Prag) im Zusammenhang mit der "
-                                                                    + "Hussiten-Bewegung der böhmische König Wenzel IV.";
-
+    public static String       NER_GER_EXAMPLE_1            = "Nachdem infolge von Streitigkeiten an der (Karls-UniversitÃ¤t Prag) im Zusammenhang mit der "
+                                                                    + "Hussiten-Bewegung der bÃ¶hmische KÃ¶nig Wenzel IV.";
+    
+    public static String       NER_ES_EXAMPLE_1             = "Leipzig es la cuna de muchas personalidades famosas, tales como el filósofo y científico Gottfried Wilhelm Leibniz (1646), " + 
+    																"el compositor Richard Wagner (1813), el historiador de arte Nikolaus Pevsner (1902), el tipógrafo Jan Tschichold (1902), el pintor Max Beckmann o el socialista Karl Liebknecht (1871).";
+ 
     public static String       RE_EN_EXAMPLE_1              =
                                                                     "The philosopher and mathematician Gottfried Wilhelm Leibniz was born in Leipzig in 1646, and attended the university from 1661 to 1666. "
                                                                             + "Nobel Prize laureate Werner Heisenberg worked here as a physics professor (from 1927 to 1942), "
