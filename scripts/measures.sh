568f5767415ba7ed189c0fd3f34bf727a8473ca1
renespeck <speckrene@gmail.com>
2015-05-27 13:27:37 +0200

    Adds helper script for measure calculation.

---
+++ scripts/measures.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+#
+# That script helps you to calculate the measures. 
+# Please change the variables 'input' and 'output' to your needs.
+# And run the script in the release folder.
+#
+input=eval/MultilayerPerceptron_total.csv
+output=eval/Measures_MultilayerPerceptron_total.csv
+#
+#
+java -cp fox-2.3.0-jar-with-dependencies.jar org.aksw.fox.utils.evaluation.FoxEvaluationHelper -i$input -o$output -mfalse
+echo "Done. Check the $output file."
