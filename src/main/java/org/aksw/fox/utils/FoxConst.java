9f65043be70a8408e6b75b9ebc93724114a3f555
Alexander Pirogov <randal.graves@yahoo.de>
2015-06-09 00:06:56 +0200

    Demo
    Setiing spanish as default for demo

--- src/main/java/org/aksw/fox/utils/FoxConst.java
+++ src/main/java/org/aksw/fox/utils/FoxConst.java
@@ -28,10 +28,19 @@ public class FoxConst {
     public static String       NER_GER_EXAMPLE_1            = "Nachdem infolge von Streitigkeiten an der (Karls-UniversitÃ¤t Prag) im Zusammenhang mit der "
                                                                     + "Hussiten-Bewegung der bÃ¶hmische KÃ¶nig Wenzel IV.";
     
-    public static String       NER_ES_EXAMPLE_1             = "Leipzig es la cuna de muchas personalidades famosas, tales como el filósofo y científico Gottfried Wilhelm Leibniz (1646), " + 
+    public static String       NER_ES_EXAMPLE_1             = "Leipzig (en alemán estándar [ˈlaɪpʦɪç]) o Lipsia, en español, es una ciudad alemana en el noroeste del estado de Sajonia. " + 
+    																"Con más de 520 000 habitantes1 a finales de 2012 es la segunda ciudad más poblada de ese estado federado (tras Dresde, " +
+    																"la capital de Sajonia) y tras Berlín, la tercera de todo el este de Alemania. Ya en el año 1165 recibió el derecho de " +
+    																"ciudad y de mercado (ahora recinto ferial). Desde entonces, Leipzig se convirtió en uno de los centros de comercio más " +
+    																"importantes de la Europa central. La ciudad tiene una larga tradición de recinto ferial y una de las ferias más antiguas (1190) de Europa. " +
+    																"Junto con Fráncfort del Meno es Leipzig el centro histórico de la imprenta y el comercio. Además, cuenta con una de las universidades " +
+    																"-tanto clásica como de música- más antiguas de Alemania. En las últimas décadas, Leipzig fue el centro de las manifestaciones de los " +
+    																"lunes (Montagsdemonstrationen) de 1989, las cuales dieron el impulso necesario a la reunificación alemana. La ciudad de Leipzig " +
+    																"tiene una gran tradición musical, la cual debe agradecer, entre otros, a Johann Sebastian Bach, Richard Wagner y Felix Mendelssohn. " +
+    																"La Orquesta de la Gewandhaus y el Coro de Santo Tomás han sólo engrandecido la fama de la ciudad en el mundo de la música. Leipzig es " +
+    																"la cuna de muchas personalidades famosas, tales como el filósofo y científico Gottfried Wilhelm Leibniz (1646), " + 
     																"el compositor Richard Wagner (1813), el historiador de arte Nikolaus Pevsner (1902), el tipógrafo Jan Tschichold (1902), " +
     																"el pintor Max Beckmann o el socialista Karl Liebknecht (1871).";
- 
     public static String       RE_EN_EXAMPLE_1              =
                                                                     "The philosopher and mathematician Gottfried Wilhelm Leibniz was born in Leipzig in 1646, and attended the university from 1661 to 1666. "
                                                                             + "Nobel Prize laureate Werner Heisenberg worked here as a physics professor (from 1927 to 1942), "
