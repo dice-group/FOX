42441750aa2cbc2dc9bcfdcf987c8c8c83995a02
renespeck <speckrene@gmail.com>
2015-08-06 17:00:39 +0200

    Adds general tools.

---
+++ src/main/java/org/aksw/fox/tools/ner/BalieTool.java
@@ -0,0 +1,101 @@
+package org.aksw.fox.tools.ner;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.aksw.fox.data.Entity;
+import org.aksw.fox.data.EntityClassMap;
+import org.aksw.fox.tools.ner.en.NERBalie;
+import org.aksw.fox.utils.FoxCfg;
+import org.aksw.fox.utils.FoxConst;
+import org.apache.log4j.PropertyConfigurator;
+
+import ca.uottawa.balie.Balie;
+import ca.uottawa.balie.DisambiguationRulesNerf;
+import ca.uottawa.balie.LexiconOnDisk;
+import ca.uottawa.balie.NamedEntityRecognitionNerfEXT;
+import ca.uottawa.balie.NamedEntityTypeEnumMappingNerf;
+import ca.uottawa.balie.PriorCorrectionNerf;
+import ca.uottawa.balie.Token;
+import ca.uottawa.balie.TokenList;
+import ca.uottawa.balie.Tokenizer;
+
+public class BalieTool extends AbstractNER {
+    // http://github.com/smtlaissezfaire/balie
+
+    protected String lang = null; ;
+
+    /**
+     * English version.
+     */
+    public BalieTool() {
+        this(Balie.LANGUAGE_ENGLISH);
+    }
+
+    public BalieTool(String lang) {
+        this.lang = lang;
+        entityClasses.put("ORGANIZATION", EntityClassMap.O);
+        entityClasses.put("LOCATION", EntityClassMap.L);
+        entityClasses.put("PERSON", EntityClassMap.P);
+        entityClasses.put("nothing", EntityClassMap.N);
+    }
+
+    public void setLang(String lang) {
+        this.lang = lang;
+    }
+
+    @Override
+    public List<Entity> retrieve(String input) {
+        LOG.info("retrieve ...");
+
+        Tokenizer tokenizer = new Tokenizer(lang, true);
+        tokenizer.Reset(); // ?
+        tokenizer.Tokenize(input);
+        PriorCorrectionNerf pcn = new PriorCorrectionNerf();
+        NamedEntityRecognitionNerfEXT ner = new NamedEntityRecognitionNerfEXT(
+                tokenizer.GetTokenList(),
+                new LexiconOnDisk(LexiconOnDisk.Lexicon.OPEN_SOURCE_LEXICON), DisambiguationRulesNerf.Load(),
+                pcn,
+                NamedEntityTypeEnumMappingNerf.values(),
+                LOG.isTraceEnabled());
+
+        ner.RecognizeEntities();
+        TokenList tokenList = ner.GetTokenList();
+
+        List<Entity> list = new ArrayList<>();
+        String lastType = null;
+        for (int i = 0; i < tokenList.Size(); i++) {
+
+            float re = Entity.DEFAULT_RELEVANCE;
+            if (FoxCfg.get("balieDefaultRelevance") != null && !Boolean.valueOf(FoxCfg.get("balieDefaultRelevance"))) {
+                Double like = ner.map.get(i);
+                if (like != null) {
+                    re = like.floatValue();
+                }
+            }
+            Token token = tokenList.Get(i);
+            String type = token.EntityType().GetLabel(NamedEntityTypeEnumMappingNerf.values());
+            if (type != null && mapTypeToSupportedType(type) != EntityClassMap.getNullCategory() && type.equals(lastType) && list.size() > 0) {
+
+                list.get(list.size() - 1).addText(token.Raw());
+
+            } else {
+                if (mapTypeToSupportedType(type) != EntityClassMap.getNullCategory())
+                    list.add(getEntity(token.Raw(), mapTypeToSupportedType(type), re, getToolName()));
+                lastType = type;
+            }
+        }
+        // TRACE
+        if (LOG.isTraceEnabled()) {
+            LOG.trace(list);
+        } // TRACE
+        return list;
+    }
+
+    public static void main(String[] a) {
+        PropertyConfigurator.configure(FoxCfg.LOG_FILE);
+        for (Entity e : new NERBalie().retrieve(FoxConst.NER_EN_EXAMPLE_1))
+            NERBalie.LOG.info(e);
+    }
+
+}
---
+++ src/main/java/org/aksw/fox/tools/ner/OpenNLPTool.java
@@ -0,0 +1,103 @@
+package org.aksw.fox.tools.ner;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
+
+import opennlp.tools.namefind.NameFinderME;
+import opennlp.tools.namefind.TokenNameFinderModel;
+import opennlp.tools.util.Span;
+
+import org.aksw.fox.data.Entity;
+import org.aksw.fox.data.EntityClassMap;
+import org.aksw.fox.utils.FoxCfg;
+import org.aksw.fox.utils.FoxTextUtil;
+
+public class OpenNLPTool extends AbstractNER {
+
+    protected String[]               modelPath;
+    protected TokenNameFinderModel[] tokenNameFinderModels;
+
+    @SuppressWarnings("unused")
+    private OpenNLPTool() {
+    };
+
+    public OpenNLPTool(String[] modelPath) {
+
+        entityClasses.put("location", EntityClassMap.L);
+        entityClasses.put("organization", EntityClassMap.O);
+        entityClasses.put("person", EntityClassMap.P);
+
+        this.modelPath = modelPath;
+
+        tokenNameFinderModels = new TokenNameFinderModel[modelPath.length];
+        InputStream[] modelIn = new InputStream[modelPath.length];
+
+        for (int i = 0; i < tokenNameFinderModels.length; i++) {
+            try {
+                modelIn[i] = new FileInputStream(modelPath[i]);
+                if (modelIn[i] != null)
+                    tokenNameFinderModels[i] = new TokenNameFinderModel(modelIn[i]);
+
+            } catch (IOException e) {
+                LOG.error("\n", e);
+            } finally {
+                try {
+                    if (modelIn[i] != null)
+                        modelIn[i].close();
+                } catch (IOException e) {
+                    LOG.error("\n", e);
+                }
+            }
+        }
+    }
+
+    // TODO: do parallel for each model
+    @Override
+    public List<Entity> retrieve(String input) {
+        LOG.info("retrieve ...");
+
+        List<Entity> list = new ArrayList<>();
+        String[] sentences = FoxTextUtil.getSentences(input);
+        LOG.debug("sentences: " + sentences.length);
+
+        for (int i = 0; i < tokenNameFinderModels.length; i++) {
+            if (tokenNameFinderModels[i] != null) {
+                NameFinderME nameFinder = new NameFinderME(tokenNameFinderModels[i]);
+                for (String sentence : sentences) {
+                    String[] tokens = FoxTextUtil.getSentenceToken(sentence);
+                    LOG.debug("tokens: " + tokens.length);
+                    if (tokens.length > 0 && tokens[tokens.length - 1].trim().isEmpty())
+                        tokens[tokens.length - 1] = ".";
+
+                    Span[] nameSpans = nameFinder.find(tokens);
+                    double[] probs = nameFinder.probs(nameSpans);
+                    for (int ii = 0; ii < nameSpans.length; ii++) {
+                        Span span = nameSpans[ii];
+
+                        String word = "";
+                        for (int j = 0; j < span.getEnd() - span.getStart(); j++)
+                            word += tokens[span.getStart() + j] + " ";
+                        word = word.trim();
+
+                        float p = Entity.DEFAULT_RELEVANCE;
+                        if (FoxCfg.get("openNLPDefaultRelevance") != null && !Boolean.valueOf(FoxCfg.get("openNLPDefaultRelevance")))
+                            p = Double.valueOf(probs[ii]).floatValue();
+                        String cl = mapTypeToSupportedType(span.getType());
+                        if (cl != EntityClassMap.getNullCategory())
+                            list.add(getEntity(word, cl, p, getToolName()));
+                    }
+                }
+                nameFinder.clearAdaptiveData();
+            }
+        }
+        // TRACE
+        if (LOG.isTraceEnabled()) {
+            LOG.trace(list);
+        } // TRACE
+        return list;
+    }
+
+}
