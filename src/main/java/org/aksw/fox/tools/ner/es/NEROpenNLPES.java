aa955b820167979f3c0e0dd79c8a200e34ee5e1e
renespeck <speckrene@gmail.com>
2015-08-06 17:02:33 +0200

    Adds ner es.

--- src/main/java/org/aksw/fox/tools/ner/es/NEROpenNLPES.java
+++ src/main/java/org/aksw/fox/tools/ner/es/NEROpenNLPES.java
@@ -1,102 +1,21 @@
 package org.aksw.fox.tools.ner.es;
 
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.util.ArrayList;
-import java.util.List;
-
-import opennlp.tools.namefind.NameFinderME;
-import opennlp.tools.namefind.TokenNameFinderModel;
-import opennlp.tools.util.Span;
-
 import org.aksw.fox.data.Entity;
-import org.aksw.fox.data.EntityClassMap;
-import org.aksw.fox.tools.ner.AbstractNER;
+import org.aksw.fox.tools.ner.OpenNLPTool;
 import org.aksw.fox.utils.FoxCfg;
 import org.aksw.fox.utils.FoxConst;
-import org.aksw.fox.utils.FoxTextUtil;
 import org.apache.log4j.PropertyConfigurator;
 
-public class NEROpenNLPES extends AbstractNER {
+public class NEROpenNLPES extends OpenNLPTool {
 
-    private final String[]               modelPath             = { "data/openNLP/es-ner-person.bin", "data/openNLP/es-ner-location.bin", "data/openNLP/es-ner-organization.bin" };
+    static final String[] modelPath =
+                                    {
+                                    "data/openNLP/es-ner-person.bin",
+                                    "data/openNLP/es-ner-location.bin",
+                                    "data/openNLP/es-ner-organization.bin" };
 
-    private final TokenNameFinderModel[] tokenNameFinderModels = new TokenNameFinderModel[modelPath.length];
-
-    /**
-     * 
-     */
     public NEROpenNLPES() {
-        InputStream[] modelIn = new InputStream[3];
-        for (int i = 0; i < tokenNameFinderModels.length; i++) {
-            try {
-
-                modelIn[i] = new FileInputStream(modelPath[i]);
-                if (modelIn[i] != null)
-                    tokenNameFinderModels[i] = new TokenNameFinderModel(modelIn[i]);
-
-            } catch (IOException e) {
-                LOG.error("\n", e);
-            } finally {
-
-                try {
-                    if (modelIn[i] != null)
-                        modelIn[i].close();
-                } catch (IOException e) {
-                    LOG.error("\n", e);
-                }
-            }
-        }
-    }
-
-    // TODO: do parallel for each model
-    @Override
-    public List<Entity> retrieve(String input) {
-        LOG.info("retrieve ...");
-
-        List<Entity> list = new ArrayList<>();
-        String[] sentences = FoxTextUtil.getSentences(input);
-
-        for (int i = 0; i < tokenNameFinderModels.length; i++) {
-            if (tokenNameFinderModels[i] != null) {
-                NameFinderME nameFinder = new NameFinderME(tokenNameFinderModels[i]);
-                for (String sentence : sentences) {
-                    String[] tokens = FoxTextUtil.getSentenceToken(sentence);
-
-                    if (tokens.length > 0 && tokens[tokens.length - 1].trim().isEmpty())
-                        tokens[tokens.length - 1] = ".";
-
-                    // if (logger.isDebugEnabled())
-                    // for (String t : tokens)
-                    // logger.debug("token: " + t);
-
-                    Span[] nameSpans = nameFinder.find(tokens);
-                    double[] probs = nameFinder.probs(nameSpans);
-                    for (int ii = 0; ii < nameSpans.length; ii++) {
-                        Span span = nameSpans[ii];
-
-                        String word = "";
-                        for (int j = 0; j < span.getEnd() - span.getStart(); j++)
-                            word += tokens[span.getStart() + j] + " ";
-                        word = word.trim();
-
-                        float p = Entity.DEFAULT_RELEVANCE;
-                        if (FoxCfg.get("openNLPDefaultRelevance") != null && !Boolean.valueOf(FoxCfg.get("openNLPDefaultRelevance")))
-                            p = Double.valueOf(probs[ii]).floatValue();
-                        String cl = EntityClassMap.openNLP(span.getType());
-                        if (cl != EntityClassMap.getNullCategory())
-                            list.add(getEntity(word, cl, p, getToolName()));
-                    }
-                }
-                nameFinder.clearAdaptiveData();
-            }
-        }
-        // TRACE
-        if (LOG.isTraceEnabled()) {
-            LOG.trace(list);
-        } // TRACE
-        return list;
+        super(modelPath);
     }
 
     public static void main(String[] a) {
--- src/main/java/org/aksw/fox/tools/ner/es/NERStanfordES.java
+++ src/main/java/org/aksw/fox/tools/ner/es/NERStanfordES.java
@@ -1,94 +1,51 @@
 package org.aksw.fox.tools.ner.es;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Properties;
 
 import org.aksw.fox.data.Entity;
 import org.aksw.fox.data.EntityClassMap;
-import org.aksw.fox.tools.ner.AbstractNER;
+import org.aksw.fox.tools.ner.StanfordTool;
+import org.aksw.fox.tools.ner.ger.NERBalieDE;
 import org.aksw.fox.utils.FoxCfg;
 import org.aksw.fox.utils.FoxConst;
 import org.apache.log4j.PropertyConfigurator;
-
-import edu.stanford.nlp.ling.CoreAnnotations.NamedEntityTagAnnotation;
-import edu.stanford.nlp.ling.CoreAnnotations.SentencesAnnotation;
-import edu.stanford.nlp.ling.CoreAnnotations.TokensAnnotation;
-import edu.stanford.nlp.ling.CoreLabel;
-import edu.stanford.nlp.pipeline.Annotation;
-import edu.stanford.nlp.pipeline.StanfordCoreNLP;
-import edu.stanford.nlp.util.CoreMap;
 
 /**
  * 
  * @author rspeck
  * 
  */
-public class NERStanfordES extends AbstractNER {
+public class NERStanfordES extends StanfordTool {
     // https://github.com/stanfordnlp/CoreNLP/blob/master/src/edu/stanford/nlp/pipeline/StanfordCoreNLP-spanish.properties
-    Properties      props    = new Properties();
-    StanfordCoreNLP pipeline = null;
-
-    public NERStanfordES() {
+    private static Properties props = new Properties();
+    static {
         /*
-         props.setProperty("annotators","tokenize, ssplit, pos, lemma, ner, parse");
-         */
+        props.setProperty("annotators","tokenize, ssplit, pos, lemma, ner, parse");
+        */
         props.setProperty("annotators", "tokenize, ssplit, pos, lemma, ner");
         props.setProperty("tokenize.language", "es");
         props.setProperty("pos.model", "edu/stanford/nlp/models/pos-tagger/spanish/spanish-distsim.tagger");
         props.setProperty("ner.model", "edu/stanford/nlp/models/ner/spanish.ancora.distsim.s512.crf.ser.gz");
         props.setProperty("ner.applyNumericClassifiers", "false");
         props.setProperty("ner.useSUTime", "false");
-        props.setProperty("parse.model", "edu/stanford/nlp/models/lexparser/spanishPCFG.ser.gz");
-        pipeline = new StanfordCoreNLP(props);
+        /*props.setProperty("parse.model", "edu/stanford/nlp/models/lexparser/spanishPCFG.ser.gz");*/
+
+    }
+
+    public NERStanfordES() {
+        super(props);
+
+        entityClasses.put("ORG", EntityClassMap.O);
+        entityClasses.put("LUG", EntityClassMap.L);
+        entityClasses.put("PERS", EntityClassMap.P);
+        entityClasses.put("O", EntityClassMap.N);
+        entityClasses.put("OTROS", EntityClassMap.N);
     }
 
     public static void main(String[] a) {
         PropertyConfigurator.configure(FoxCfg.LOG_FILE);
-        NERStanfordES n = new NERStanfordES();
-        n.retrieve(FoxConst.NER_ES_EXAMPLE_1);
+        for (Entity e : new NERStanfordES().retrieve(FoxConst.NER_ES_EXAMPLE_1))
+            NERBalieDE.LOG.info(e);
     }
 
-    @Override
-    public List<Entity> retrieve(String text) {
-        LOG.info("retrieve ...");
-        Annotation ann = new Annotation(text);
-        pipeline.annotate(ann);
-
-        List<Entity> list = new ArrayList<>();
-
-        for (CoreMap sentence : ann.get(SentencesAnnotation.class)) {
-            String tokensentence = "";
-            for (CoreLabel token : sentence.get(TokensAnnotation.class)) {
-            System.out.println(token);
-                tokensentence += token.word() + " ";
-                String type = EntityClassMap.stanfordes(token.get(NamedEntityTagAnnotation.class));
-                String currentToken = token.originalText();
-                // check for multiword entities
-                boolean contains = false;
-                boolean equalTypes = false;
-                Entity lastEntity = null;
-                //System.out.println(token.get(NamedEntityTagAnnotation.class));
-                if (!list.isEmpty()) {
-                    lastEntity = list.get(list.size() - 1);
-                    contains = tokensentence.contains(lastEntity.getText() + " " + currentToken + " ");
-                    equalTypes = type.equals(lastEntity.getType());
-                }
-                if (contains && equalTypes) {
-                    lastEntity.addText(currentToken);
-                } else {
-                    if (type != EntityClassMap.getNullCategory()) {
-                        float p = Entity.DEFAULT_RELEVANCE;
-                        list.add(getEntity(currentToken, type, p, getToolName()));
-                    }
-                }
-            }
-        }
-        // TRACE
-        if (LOG.isTraceEnabled()) {
-            LOG.trace(list);
-        } // TRACE
-        LOG.info("retrieve done.");
-        return list;
-    }
 }
