9dcf52a1c3219a2fa98a01977225b945aeb6e4f5
renespeck <speckrene@gmail.com>
2015-08-10 10:15:05 +0200

    Adds Exceptions.

---
+++ src/main/java/org/aksw/fox/data/exception/PortInUseException.java
@@ -0,0 +1,9 @@
+package org.aksw.fox.data.exception;
+
+public class PortInUseException extends Exception {
+    private static final long serialVersionUID = 8440331545375107244L;
+
+    public PortInUseException(int port) {
+        super("Port " + port + " in use or wrong argument, try another one!");
+    }
+}
---
+++ src/main/java/org/aksw/fox/data/exception/UnsupportedLangException.java
@@ -0,0 +1,14 @@
+package org.aksw.fox.data.exception;
+
+/**
+ * 
+ * @author rspeck
+ *
+ */
+public class UnsupportedLangException extends Exception {
+    private static final long serialVersionUID = 4848141839425909601L;
+
+    public UnsupportedLangException(String m) {
+        super(m);
+    }
+}
