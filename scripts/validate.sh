d2bb364d42c6212d7a2463c944b7b3914b8b02c5
renespeck <speckrene@gmail.com>
2015-08-12 10:03:41 +0200

    Update scripts.

--- scripts/validate.sh
+++ scripts/validate.sh
@@ -1,1 +1,1 @@
-nohup java -Xmx5G -cp fox-2.3.0-jar-with-dependencies.jar org.aksw.fox.MainFox -avalidate -iinput/1 > validate.log &
+nohup java -Xmx5G -cp fox-2.3.0-jar-with-dependencies.jar org.aksw.fox.FoxCLI -avalidate -iinput/1 > validate.log &
