5508ee764b9ad4f829797c400982b59ddd77d88b
renespeck <speckrene@gmail.com>
2015-08-22 12:28:31 +0200

    Updates scripts.

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,1 +1,1 @@
-nohup java -Xmx10G -cp fox-2.3.0-jar-with-dependencies.jar org.aksw.fox.FoxRESTful > run.log &
+nohup java -Xmx8G -cp fox-2.3.0-jar-with-dependencies.jar org.aksw.fox.FoxRESTful > run.log &
--- scripts/validate.sh
+++ scripts/validate.sh
@@ -1,1 +1,1 @@
-nohup java -Xmx5G -cp fox-2.3.0-jar-with-dependencies.jar org.aksw.fox.FoxCLI -avalidate -iinput/1 > validate.log &
+nohup java -Xmx8G -cp fox-2.3.0-jar-with-dependencies.jar org.aksw.fox.FoxCLI -len -avalidate -iinput/1 > validate.log &
