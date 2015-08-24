c954340b2e01a4a10b1c4195c30c3489a72a28cd
renespeck <speckrene@gmail.com>
2015-08-24 02:12:13 +0200

    Adds more tests.

--- src/test/java/org/aksw/fox/web/TestServer.java
+++ src/test/java/org/aksw/fox/web/TestServer.java
@@ -1,19 +1,28 @@
 package org.aksw.fox.web;
 
 import java.io.IOException;
-import java.lang.reflect.InvocationTargetException;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.nio.charset.Charset;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Map;
 import java.util.Map.Entry;
 
-import org.aksw.fox.data.exception.LoadingNotPossibleException;
-import org.aksw.fox.data.exception.PortInUseException;
-import org.aksw.fox.data.exception.UnsupportedLangException;
+import org.aksw.fox.Fox;
+import org.aksw.fox.utils.FoxConst;
+import org.apache.commons.io.IOUtils;
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.fluent.Request;
+import org.apache.http.client.fluent.Response;
+import org.apache.http.entity.ContentType;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.util.EntityUtils;
+import org.apache.jena.riot.Lang;
 import org.apache.log4j.LogManager;
 import org.apache.log4j.Logger;
+import org.json.JSONObject;
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -23,32 +32,18 @@ public class TestServer {
 
     private Server             server      = null;
 
-    long                       waitingTime = 5 * 60 * 1000;                         // ms
+    long                       waitingTime = 10 * 60 * 1000;                        // 5m
 
     @Test
     public void serverTest() {
-
-        // fox.properties need to be available
-        // Assert.assertTrue(FoxCfg.loadFile("fox.properties-dist"));
-        // Assert.assertNotNull(FoxCfg.get(Server.CFG_KEY_POOL_SIZE));
-
         // start server
         long startTime = System.currentTimeMillis();
         new Thread(new Runnable() {
             public void run() {
                 try {
                     server = new Server();
-                } catch (IllegalArgumentException | InvocationTargetException | NoSuchMethodException | SecurityException e) {
+                } catch (Exception e) {
                     LOG.error(e.getLocalizedMessage(), e);
-                } catch (LoadingNotPossibleException e) {
-                    // TODO Auto-generated catch block
-                    e.printStackTrace();
-                } catch (UnsupportedLangException e) {
-                    // TODO Auto-generated catch block
-                    e.printStackTrace();
-                } catch (PortInUseException e) {
-                    // TODO Auto-generated catch block
-                    e.printStackTrace();
                 }
                 Assert.assertTrue(server.start());
             }
@@ -65,16 +60,54 @@ public class TestServer {
 
         // info
         if (Server.running) {
-            long elapsedTime = System.currentTimeMillis() - startTime;
-            LOG.info("server started in " + (elapsedTime / 1000L) + "s");
+            LOG.info("server started");
+
+            apiTest();
+            demoTest();
+
+            // stop server
+            server.stop();
         } else {
-            LOG.warn("Server start toke too long (" + (waitingTime / 1000L) + "s)!");
+            LOG.warn("Server start toke too long");
         }
+    }
 
-        demoTest();
+    /**
+     * HTTP GET test
+     */
+    public void apiTest() {
+        LOG.info("apiTest ...");
 
-        // stop server
-        server.stop();
+        try {
+            final Charset UTF_8 = Charset.forName("UTF-8");
+            Response response = Request
+                    .Post("http://" + server.host + ":" + server.port + "/call/ner/entities")
+                    .addHeader("Content-type", "application/json;charset=".concat(UTF_8.toString()))
+                    .addHeader("Accept-Charset", UTF_8.toString())
+                    .body(new StringEntity(
+                            new JSONObject()
+                                    .put(Fox.Parameter.TYPE.toString(), Fox.Type.TEXT.toString())
+                                    .put(Fox.Parameter.LANG.toString(), Fox.Langs.EN.toString())
+                                    .put(Fox.Parameter.TASK.toString(), Fox.Task.NER.toString())
+                                    .put(Fox.Parameter.OUTPUT.toString(), Lang.TURTLE.getName())
+                                    .put(Fox.Parameter.INPUT.toString(), FoxConst.NER_EN_EXAMPLE_1)
+                                    .toString(),
+                            ContentType.APPLICATION_JSON
+                            ))
+                    .execute();
+
+            HttpResponse httpResponse = response.returnResponse();
+            LOG.info(httpResponse.getStatusLine().toString());
+            HttpEntity entry = httpResponse.getEntity();
+            String r = IOUtils.toString(entry.getContent(), UTF_8);
+            EntityUtils.consume(entry);
+            LOG.info(r);
+            Assert.assertNotNull(r);
+            Assert.assertFalse(r.isEmpty());
+
+        } catch (IOException e) {
+            LOG.error(e.getLocalizedMessage(), e);
+        }
     }
 
     /**
@@ -108,6 +141,5 @@ public class TestServer {
         } catch (IOException e) {
             LOG.error(e.getLocalizedMessage(), e);
         }
-
     }
 }
