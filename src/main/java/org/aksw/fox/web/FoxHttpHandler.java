f1c1d19ddfb52f32ea47488cf18b15b0fc3683f6
renespeck <speckrene@gmail.com>
2016-02-26 17:05:09 +0100

    Close #23.

--- src/main/java/org/aksw/fox/web/FoxHttpHandler.java
+++ src/main/java/org/aksw/fox/web/FoxHttpHandler.java
@@ -21,140 +21,154 @@ import org.jetlang.fibers.ThreadFiber;
 import org.json.JSONObject;
 
 /**
- * 
+ *
  * @author rspeck
- * 
+ *
  */
 public class FoxHttpHandler extends AbstractFoxHttpHandler {
 
-    public static final String CFG_KEY_FOX_LIFETIME = FoxHttpHandler.class.getName().concat(".lifetime");
-    FoxLanguageDetector        languageDetector     = new FoxLanguageDetector();
+  public static final String CFG_KEY_FOX_LIFETIME =
+      FoxHttpHandler.class.getName().concat(".lifetime");
+  FoxLanguageDetector languageDetector = new FoxLanguageDetector();
 
-    @Override
-    protected void postService(Request request, Response response, Map<String, String> parameter) {
+  @Override
+  protected void postService(final Request request, final Response response,
+      final Map<String, String> parameter) {
 
-        switch (parameter.get(Fox.Parameter.TYPE.toString()).toString()) {
-        case "url":
-            parameter.put(Fox.Parameter.INPUT.toString(), FoxTextUtil.urlToText(parameter.get(Fox.Parameter.INPUT.toString())));
-            break;
-        case "text":
-            parameter.put(Fox.Parameter.INPUT.toString(), FoxTextUtil.htmlToText(parameter.get(Fox.Parameter.INPUT.toString())));
-            break;
-        }
-
-        String lang = parameter.get(Fox.Parameter.LANG.toString());
-        Fox.Langs l = Fox.Langs.fromString(lang);
-        if (l == null) {
-            l = languageDetector.detect(parameter.get(Fox.Parameter.INPUT.toString()));
-            if (l != null)
-                lang = l.toString();
-            else
-                lang = "";
-        }
-        LOG.info("lang: " + lang);
-        if (!lang.isEmpty()) {
-            LOG.info(Server.pool);
-            // get a fox instance
-            IFox fox = Server.pool.get(lang).poll();
-            LOG.info(fox);
-            if (fox != null) {
-
-                // init. thread
-                Fiber fiber = new ThreadFiber();
-                fiber.start();
-                final CountDownLatch latch = new CountDownLatch(1);
-                fox.setCountDownLatch(latch);
-                fox.setParameter(parameter);
-                fiber.execute(fox);
-
-                // wait
-                try {
-                    latch.await(Integer.parseInt(FoxCfg.get(CFG_KEY_FOX_LIFETIME)), TimeUnit.MINUTES);
-                } catch (InterruptedException e) {
-                    LOG.error("Fox timeout after " + FoxCfg.get(CFG_KEY_FOX_LIFETIME) + "min.");
-                    LOG.error("\n", e);
-                    LOG.error("input: " + parameter.get(Fox.Parameter.INPUT.toString()));
-                }
-
-                // shutdown thread
-                fiber.dispose();
-
-                // get output
-                String output = "";
-                if (latch.getCount() == 0) {
-                    output = fox.getResults();
-                    Server.pool.get(lang).push(fox);
-                } else {
-                    fox = null;
-                    Server.pool.get(lang).add();
-                    // TODO : error output
-                }
-
-                String in = null, out = null, log = null;
-                if (fox != null) {
-                    in = FoxStringUtil.encodeURLComponent(parameter.get(Fox.Parameter.INPUT.toString()));
-                    out = FoxStringUtil.encodeURLComponent(output);
-                    log = FoxStringUtil.encodeURLComponent(fox.getLog());
-                }
-                setResponse(
-                        response,
-                        new JSONObject()
-                                .put("input", in == null ? "" : in)
-                                .put("output", out == null ? "" : out)
-                                .put("log", log == null ? "" : log)
-                                .toString(),
-                        HttpURLConnection.HTTP_OK,
-                        "application/json");
-            } else
-                LOG.warn("Could not found fox from pool");
-        } else
-            setResponse(
-                    response,
-                    new JSONObject().toString(),
-                    HttpURLConnection.HTTP_BAD_REQUEST,
-                    "application/json");
+    String errormessage = "";
+    switch (parameter.get(Fox.Parameter.TYPE.toString()).toString()) {
+      case "url":
+        parameter.put(Fox.Parameter.INPUT.toString(),
+            FoxTextUtil.urlToText(parameter.get(Fox.Parameter.INPUT.toString())));
+        break;
+      case "text":
+        parameter.put(Fox.Parameter.INPUT.toString(),
+            FoxTextUtil.htmlToText(parameter.get(Fox.Parameter.INPUT.toString())));
+        break;
     }
 
-    @Override
-    protected boolean checkParameter(Map<String, String> formData) {
+    String lang = parameter.get(Fox.Parameter.LANG.toString());
+    Fox.Langs l = Fox.Langs.fromString(lang);
+    if (l == null) {
+      l = languageDetector.detect(parameter.get(Fox.Parameter.INPUT.toString()));
+      if (l != null) {
+        lang = l.toString();
+      } else {
+        lang = "";
+      }
+    }
+    LOG.info("lang: " + lang);
+    if (!lang.isEmpty()) {
 
-        LOG.info("checking form parameter ...");
+      // get a fox instance
+      final Pool<IFox> pool = Server.pool.get(lang);
+      IFox fox = null;
+      if (pool != null) {
+        fox = pool.poll();
+      }
 
-        String type = formData.get(Fox.Parameter.TYPE.toString());
-        if (type == null || !(type.equalsIgnoreCase(Fox.Type.URL.toString()) || type.equalsIgnoreCase(Fox.Type.TEXT.toString())))
-            return false;
+      if (fox != null) {
 
-        String text = formData.get(Fox.Parameter.INPUT.toString());
-        if (text == null || text.trim().isEmpty())
-            return false;
+        // init. thread
+        final Fiber fiber = new ThreadFiber();
+        fiber.start();
+        final CountDownLatch latch = new CountDownLatch(1);
+        fox.setCountDownLatch(latch);
+        fox.setParameter(parameter);
+        fiber.execute(fox);
 
-        String task = formData.get(Fox.Parameter.TASK.toString());
-        if (task == null || !(task.equalsIgnoreCase("ke") || task.equalsIgnoreCase("ner") ||
-                task.equalsIgnoreCase("keandner") || task.equalsIgnoreCase("re") || task.equalsIgnoreCase("all")))
-            return false;
-
-        String output = formData.get(Fox.Parameter.OUTPUT.toString());
-
-        if (!FoxJena.prints.contains(output))
-            return false;
-
-        String nif = formData.get(Fox.Parameter.NIF.toString());
-        if (nif == null || !nif.equalsIgnoreCase("true"))
-            formData.put(Fox.Parameter.NIF.toString(), "false");
-        else
-            formData.put(Fox.Parameter.NIF.toString(), "true");
-
-        String foxlight = formData.get(Fox.Parameter.FOXLIGHT.toString());
-        if (foxlight == null || foxlight.equalsIgnoreCase("off")) {
-            formData.put(Fox.Parameter.FOXLIGHT.toString(), "OFF");
+        // wait
+        try {
+          latch.await(Integer.parseInt(FoxCfg.get(CFG_KEY_FOX_LIFETIME)), TimeUnit.MINUTES);
+        } catch (final InterruptedException e) {
+          LOG.error("Fox timeout after " + FoxCfg.get(CFG_KEY_FOX_LIFETIME) + "min.");
+          LOG.error("\n", e);
+          LOG.error("input: " + parameter.get(Fox.Parameter.INPUT.toString()));
         }
 
-        LOG.info("ok.");
-        return true;
+        // shutdown thread
+        fiber.dispose();
+
+        // get output
+        String output = "";
+        if (latch.getCount() == 0) {
+          output = fox.getResults();
+          Server.pool.get(lang).push(fox);
+        } else {
+          fox = null;
+          Server.pool.get(lang).add();
+        }
+
+        String in = null, out = null, log = null;
+        if (fox != null) {
+          in = FoxStringUtil.encodeURLComponent(parameter.get(Fox.Parameter.INPUT.toString()));
+          out = FoxStringUtil.encodeURLComponent(output);
+          log = FoxStringUtil.encodeURLComponent(fox.getLog());
+        }
+        setResponse(response,
+            new JSONObject().put("input", in == null ? "" : in)
+                .put("output", out == null ? "" : out).put("log", log == null ? "" : log)
+                .toString(),
+            HttpURLConnection.HTTP_OK, "application/json");
+        return;
+      } else {
+        errormessage = "Could not found fox from pool with lang: " + lang;
+        LOG.warn(errormessage);
+      }
     }
 
-    @Override
-    public List<String> getMappings() {
-        return Arrays.asList("/api");
+    setResponse(response, new JSONObject().put("error", errormessage).toString(),
+        HttpURLConnection.HTTP_BAD_REQUEST, "application/json");
+    return;
+  }
+
+  @Override
+  protected boolean checkParameter(final Map<String, String> formData) {
+
+    LOG.info("checking form parameter ...");
+
+    final String type = formData.get(Fox.Parameter.TYPE.toString());
+    if ((type == null) || !(type.equalsIgnoreCase(Fox.Type.URL.toString())
+        || type.equalsIgnoreCase(Fox.Type.TEXT.toString()))) {
+      return false;
     }
+
+    final String text = formData.get(Fox.Parameter.INPUT.toString());
+    if ((text == null) || text.trim().isEmpty()) {
+      return false;
+    }
+
+    final String task = formData.get(Fox.Parameter.TASK.toString());
+    if ((task == null) || !(task.equalsIgnoreCase("ke") || task.equalsIgnoreCase("ner")
+        || task.equalsIgnoreCase("keandner") || task.equalsIgnoreCase("re")
+        || task.equalsIgnoreCase("all"))) {
+      return false;
+    }
+
+    final String output = formData.get(Fox.Parameter.OUTPUT.toString());
+
+    if (!FoxJena.prints.contains(output)) {
+      return false;
+    }
+
+    final String nif = formData.get(Fox.Parameter.NIF.toString());
+    if ((nif == null) || !nif.equalsIgnoreCase("true")) {
+      formData.put(Fox.Parameter.NIF.toString(), "false");
+    } else {
+      formData.put(Fox.Parameter.NIF.toString(), "true");
+    }
+
+    final String foxlight = formData.get(Fox.Parameter.FOXLIGHT.toString());
+    if ((foxlight == null) || foxlight.equalsIgnoreCase("off")) {
+      formData.put(Fox.Parameter.FOXLIGHT.toString(), "OFF");
+    }
+
+    LOG.info("ok.");
+    return true;
+  }
+
+  @Override
+  public List<String> getMappings() {
+    return Arrays.asList("/api");
+  }
 }
