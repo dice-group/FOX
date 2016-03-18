86d6ffa25ec11aa767a7a2de77fc35d7b7714719
renespeck <speckrene@gmail.com>
2016-03-18 03:28:52 +0100

    Update dependencies.

--- src/main/resources/scripts/learn.sh
+++ src/main/resources/scripts/learn.sh
@@ -1,1 +1,3 @@
-nohup java -Xmx8G -cp fox-${project.version}-jar-with-dependencies.jar org.aksw.fox.FoxCLI -lde -atrain -iinput/Wikiner/aij-wikiner-de-wp3.bz2 > learn.log &
+#nohup java -Xmx8G -cp fox-${project.version}-jar-with-dependencies.jar org.aksw.fox.FoxCLI -lde -atrain -iinput/Wikiner/aij-wikiner-de-wp3.bz2 > learn.log &
+nohup java -Xmx8G -cp fox-${project.version}-jar-with-dependencies.jar org.aksw.fox.FoxCLI -len -atrain -iinput/2 > learn.log &
+
