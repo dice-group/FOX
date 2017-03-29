bf1caf35a7b07f21b8f61af3e45a2269dd5e07de
renespeck <speckrene@gmail.com>
2017-03-29 15:22:51 +0200

    Scripts update.

--- src/main/resources/scripts/run.sh
+++ src/main/resources/scripts/run.sh
@@ -1,1 +1,1 @@
-nohup java -Xmx8G -cp fox-${project.version}-jar-with-dependencies.jar org.aksw.fox.FoxRESTful > run.log &
+nohup java -Xmx8G -cp fox-${project.version}-jar-with-dependencies.jar org.aksw.fox.ui.FoxRESTful > run.log &
--- src/main/resources/scripts/validate.sh
+++ src/main/resources/scripts/validate.sh
@@ -1,1 +1,1 @@
-nohup java -Xmx8G -cp fox-${project.version}-jar-with-dependencies.jar org.aksw.fox.FoxCLI -len -avalidate -iinput/1 > validate.log &
+nohup java -Xmx8G -cp fox-${project.version}-jar-with-dependencies.jar org.aksw.fox.ui.FoxCLI -len -avalidate -iinput/1 > validate.log &
