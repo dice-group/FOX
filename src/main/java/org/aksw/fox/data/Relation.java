0fd209751c997b10563f7ae9484bd81090a074ae
renespeck <speckrene@gmail.com>
2017-03-30 11:14:36 +0200

    Update.

--- src/main/java/org/aksw/fox/data/Relation.java
+++ src/main/java/org/aksw/fox/data/Relation.java
@@ -6,61 +6,83 @@ import java.util.List;
 import org.apache.log4j.LogManager;
 import org.apache.log4j.Logger;
 
+/**
+ *
+ * @author Ren&eacute; Speck <speck@informatik.uni-leipzig.de>
+ *
+ */
 public class Relation {
-    public static Logger      LOG               = LogManager.getLogger(Relation.class);
-    public static final float DEFAULT_RELEVANCE = -1;
 
-    protected Entity          subjectEntity     = null;
-    protected String          relationLabel     = null;
-    protected String          relationByTool    = null;
+  public static Logger LOG = LogManager.getLogger(Relation.class);
 
-    protected Entity          objectEntity      = null;
-    protected List<URI>       relation          = null;
-    protected String          tool              = null;
-    protected float           relevance         = DEFAULT_RELEVANCE;
+  public static final float DEFAULT_RELEVANCE = -1;
 
-    public Relation(Entity subjectEntity, String relationLabel, String relationByTool, Entity objectEntity, List<URI> relation, String tool, float relevance) {
-        this.subjectEntity = subjectEntity;
-        this.relationLabel = relationLabel;
-        this.relationByTool = relationByTool;
-        this.objectEntity = objectEntity;
-        this.relation = relation;
-        this.tool = tool;
-        this.relevance = relevance;
-    }
+  protected Entity subjectEntity = null;
+  protected String relationLabel = null;
+  protected String relationByTool = null;
 
-    public Entity getSubjectEntity() {
-        return subjectEntity;
-    }
+  protected Entity objectEntity = null;
+  protected List<URI> relation = null;
+  protected String tool = null;
+  protected float relevance = DEFAULT_RELEVANCE;
 
-    public String getRelationLabel() {
-        return relationLabel;
-    }
+  /***
+   *
+   * Constructor.
+   *
+   * @param subjectEntity
+   * @param relationLabel
+   * @param relationByTool
+   * @param objectEntity
+   * @param relation
+   * @param tool
+   * @param relevance
+   */
+  public Relation(final Entity subjectEntity, final String relationLabel,
+      final String relationByTool, final Entity objectEntity, final List<URI> relation,
+      final String tool, final float relevance) {
 
-    public Entity getObjectEntity() {
-        return objectEntity;
-    }
+    this.subjectEntity = subjectEntity;
+    this.relationLabel = relationLabel;
+    this.relationByTool = relationByTool;
+    this.objectEntity = objectEntity;
+    this.relation = relation;
+    this.tool = tool;
+    this.relevance = relevance;
+  }
 
-    public List<URI> getRelation() {
-        return relation;
-    }
+  public Entity getSubjectEntity() {
+    return subjectEntity;
+  }
 
-    public String getRelationByTool() {
-        return relationByTool;
-    }
+  public String getRelationLabel() {
+    return relationLabel;
+  }
 
-    public String getTool() {
-        return tool;
-    }
+  public Entity getObjectEntity() {
+    return objectEntity;
+  }
 
-    public float getRelevance() {
-        return relevance;
-    }
+  public List<URI> getRelation() {
+    return relation;
+  }
 
-    @Override
-    public String toString() {
-        return "Relation [subjectEntity=" + subjectEntity + ","
-                + " relationLabel=" + relationLabel + ", objectEntity=" + objectEntity + ","
-                + " relation=" + relation + ", tool=" + tool + ", relevance=" + relevance + "]";
-    }
+  public String getRelationByTool() {
+    return relationByTool;
+  }
+
+  public String getTool() {
+    return tool;
+  }
+
+  public float getRelevance() {
+    return relevance;
+  }
+
+  @Override
+  public String toString() {
+    return "Relation [subjectEntity=" + subjectEntity + "," + " relationLabel=" + relationLabel
+        + ", objectEntity=" + objectEntity + "," + " relation=" + relation + ", tool=" + tool
+        + ", relevance=" + relevance + "]";
+  }
 }
--- src/main/java/org/aksw/fox/nerlearner/FoxClassifier.java
+++ src/main/java/org/aksw/fox/nerlearner/FoxClassifier.java
@@ -23,213 +23,224 @@ import weka.core.SerializationHelper;
 
 /**
  * FoxClassifier.
- * 
+ *
  * @author rspeck
- * 
+ *
  */
 public class FoxClassifier {
 
-    public static Logger       LOG                      = LogManager.getLogger(FoxClassifier.class);
+  public static Logger LOG = LogManager.getLogger(FoxClassifier.class);
 
-    public static final String CFG_KEY_MODEL_PATH       = FoxClassifier.class.getName().concat(".modelPath");
-    public static final String CFG_KEY_LEARNER          = FoxClassifier.class.getName().concat(".learner");
-    public static final String CFG_KEY_LEARNER_OPTIONS  = FoxClassifier.class.getName().concat(".learnerOptions");
-    public static final String CFG_KEY_LEARNER_TRAINING = FoxClassifier.class.getName().concat(".training");
+  public static final String CFG_KEY_MODEL_PATH =
+      FoxClassifier.class.getName().concat(".modelPath");
+  public static final String CFG_KEY_LEARNER = FoxClassifier.class.getName().concat(".learner");
+  public static final String CFG_KEY_LEARNER_OPTIONS =
+      FoxClassifier.class.getName().concat(".learnerOptions");
+  public static final String CFG_KEY_LEARNER_TRAINING =
+      FoxClassifier.class.getName().concat(".training");
 
-    protected Classifier       classifier               = null;
-    protected Instances        instances                = null;
-    protected FoxInstances     foxInstances             = null;
-    private boolean            isTrained                = false;
+  protected Classifier classifier = null;
+  protected Instances instances = null;
+  protected FoxInstances foxInstances = null;
+  private boolean isTrained = false;
 
-    /**
-     * FoxClassifier.
-     */
-    public FoxClassifier() {
-        LOG.info(FoxClassifier.class + " ...");
-        foxInstances = new FoxInstances();
+  /**
+   * FoxClassifier.
+   */
+  public FoxClassifier() {
+    LOG.info(FoxClassifier.class + " ...");
+    foxInstances = new FoxInstances();
+  }
+
+  /**
+   * Builds the {@link #classifier} with {@link #instances}.
+   *
+   * @throws Exception
+   */
+  protected void buildClassifier() throws Exception {
+    if (LOG.isDebugEnabled()) {
+      LOG.debug("buildClassifier ...");
     }
 
-    /**
-     * Builds the {@link #classifier} with {@link #instances}.
-     * 
-     * @throws Exception
-     */
-    protected void buildClassifier() throws Exception {
-        if (LOG.isDebugEnabled())
-            LOG.debug("buildClassifier ...");
+    if (instances != null) {
+      classifier.buildClassifier(instances);
+      isTrained = true;
+    } else {
+      LOG.error("Initialize instances first.");
+    }
+  }
 
-        if (instances != null) {
-            classifier.buildClassifier(instances);
-            isTrained = true;
-        } else {
-            LOG.error("Initialize instances first.");
+  /**
+   * Initializes {@link #instances}.
+   *
+   * @param input
+   * @param toolResults
+   * @param oracel
+   */
+  protected void initInstances(final Set<String> input, final Map<String, Set<Entity>> toolResults,
+      final Map<String, String> oracle) {
+    LOG.info("init. instances ...");
+    instances = (oracle == null) ? foxInstances.getInstances(input, toolResults)
+        : foxInstances.getInstances(input, toolResults, oracle);
+  }
+
+  protected String getName(final String lang) {
+    return FoxCfg.get(FoxClassifier.CFG_KEY_MODEL_PATH) + File.separator + lang + File.separator
+        + FoxCfg.get(FoxClassifier.CFG_KEY_LEARNER);
+  }
+
+  /**
+   *
+   * @param classifier
+   * @param file
+   */
+  public void writeClassifier(final String file, final String lang) {
+    final String name = getName(lang);
+    LOG.info("writeClassifier: " + name);
+    final String path = FilenameUtils.getPath(name);
+    try {
+      FileUtils.forceMkdir(new File(path));
+      SerializationHelper.write(name, classifier);
+    } catch (final Exception e) {
+      LOG.error(e.getLocalizedMessage(), e);
+    }
+  }
+
+  /**
+   * Reads a serialized Classifier from file that is specified in the fox properties.
+   */
+  public void readClassifier(final String lang) {
+    final String name = getName(lang);
+    LOG.info("readClassifier: " + name);
+    try {
+      classifier = (Classifier) SerializationHelper.read(name.trim());
+    } catch (final Exception e) {
+      LOG.error(e.getLocalizedMessage(), e);
+    }
+    LOG.info("readClassifier done.");
+  }
+
+  /**
+   * Rewrites results and input to labels, uses a serialized classifier to classify this labels and
+   * rewrites the labels.
+   *
+   * @param sentence
+   * @param toolResults
+   * @return classified token
+   */
+  public Set<Entity> classify(final IPostProcessing pp) {
+    LOG.info("classify ...");
+
+    // rewrite to use labels
+    initInstances(pp.getLabeledInput(), pp.getLabeledToolResults(), null);
+
+    final Instances classified = new Instances(instances);
+    for (int i = 0; i < instances.numInstances(); i++) {
+      try {
+        classified.instance(i).setClassValue(classifier.classifyInstance(instances.instance(i)));
+      } catch (final Exception e) {
+        LOG.error("\n", e);
+      }
+    }
+    // TRACE
+    if (LOG.isTraceEnabled()) {
+      LOG.trace("classified: \n" + classified);
+      // TRACE
+    }
+
+    final Set<Entity> set = pp.instancesToEntities(classified);
+    LOG.info("classify done, size: " + set.size());
+    if (LOG.isDebugEnabled()) {
+      LOG.debug(classifier);
+    }
+    return set;
+  }
+
+  /**
+   * Reads files, init. instances and builds a classifier.
+   *
+   * @param files files to read as training data
+   */
+  public void training(final String input, final Map<String, Set<Entity>> toolResults,
+      final Map<String, String> oracle) throws Exception {
+    LOG.info("training ...");
+
+    // init. training data
+    final IPostProcessing pp = new PostProcessing(new TokenManager(input), toolResults);
+    final Map<String, String> labeledOracle = pp.getLabeledMap(oracle);
+    final Map<String, Set<Entity>> labledToolResults = pp.getLabeledToolResults();
+
+    // DEBUG
+    if (LOG.isDebugEnabled()) {
+      LOG.debug("labeled entity:");
+
+      final Set<Entity> set = new LinkedHashSet<>();
+      for (final Entry<String, Set<Entity>> e : labledToolResults.entrySet()) {
+        set.addAll(e.getValue());
+      }
+
+      for (final Entity e : set) {
+        LOG.trace(e.getText());
+      }
+    }
+    // DEBUG
+
+    initInstances(pp.getLabeledInput(), labledToolResults, labeledOracle);
+    buildClassifier();
+  }
+
+  /**
+   * Evaluation
+   *
+   * @throws Exception
+   */
+  public void eva() throws Exception {
+
+    if (isTrained) {
+      final Evaluation eva = new Evaluation(instances);
+      eva.evaluateModel(classifier, instances);
+
+      // print summary
+      LOG.info("summary\n" + eva.toSummaryString());
+
+      // print the confusion matrix
+      final StringBuffer cm = new StringBuffer();
+      final double[][] cmMatrix = eva.confusionMatrix();
+      for (final String cl : EntityClassMap.entityClasses) {
+        cm.append(cl + "\t");
+      }
+      cm.append("\n");
+      for (int i = 0; i < cmMatrix.length; i++) {
+        for (int ii = 0; ii < cmMatrix[i].length; ii++) {
+          cm.append(cmMatrix[i][ii] + "\t\t");
         }
+        cm.append("\n");
+      }
+
+      LOG.info("confusion matrix\n" + cm.toString());
+
+      // measure
+      for (final String cl : EntityClassMap.entityClasses) {
+        LOG.info("class: " + cl);
+        LOG.info("fMeasure: " + eva.fMeasure(EntityClassMap.entityClasses.indexOf(cl)));
+        LOG.info("precision: " + eva.precision(EntityClassMap.entityClasses.indexOf(cl)));
+        LOG.info("recall: " + eva.recall(EntityClassMap.entityClasses.indexOf(cl)));
+      }
+
+    } else {
+      LOG.error("Build/training a classifier first.");
     }
+  }
 
-    /**
-     * Initializes {@link #instances}.
-     * 
-     * @param input
-     * @param toolResults
-     * @param oracel
-     */
-    protected void initInstances(Set<String> input, Map<String, Set<Entity>> toolResults, Map<String, String> oracle) {
-        LOG.info("init. instances ...");
-        instances = (oracle == null) ? foxInstances.getInstances(input, toolResults) : foxInstances.getInstances(input, toolResults, oracle);
-    }
+  public void setIsTrained(final boolean bool) {
+    isTrained = bool;
+  }
 
-    protected String getName(String lang) {
-        return FoxCfg.get(FoxClassifier.CFG_KEY_MODEL_PATH) + File.separator + lang + File.separator + FoxCfg.get(FoxClassifier.CFG_KEY_LEARNER);
-    }
+  public void setClassifier(final Classifier classifier) {
+    this.classifier = classifier;
+  }
 
-    /**
-     * 
-     * @param classifier
-     * @param file
-     */
-    public void writeClassifier(String file, String lang) {
-        String name = getName(lang);
-        LOG.info("writeClassifier: " + name);
-        String path = FilenameUtils.getPath(name);
-        try {
-            FileUtils.forceMkdir(new File(path));
-            SerializationHelper.write(name, classifier);
-        } catch (Exception e) {
-            LOG.error(e.getLocalizedMessage(), e);
-        }
-    }
-
-    /**
-     * Reads a serialized Classifier from file that is specified in the fox
-     * properties.
-     */
-    public void readClassifier(String lang) {
-        String name = getName(lang);
-        LOG.info("readClassifier: " + name);
-        try {
-            classifier = (Classifier) SerializationHelper.read(name.trim());
-        } catch (Exception e) {
-            LOG.error(e.getLocalizedMessage(), e);
-        }
-        LOG.info("readClassifier done.");
-    }
-
-    /**
-     * Rewrites results and input to labels, uses a serialized classifier to
-     * classify this labels and rewrites the labels.
-     * 
-     * @param sentence
-     * @param toolResults
-     * @return classified token
-     */
-    public Set<Entity> classify(IPostProcessing pp) {
-        LOG.info("classify ...");
-
-        // rewrite to use labels
-        initInstances(pp.getLabeledInput(), pp.getLabeledToolResults(), null);
-
-        Instances classified = new Instances(instances);
-        for (int i = 0; i < instances.numInstances(); i++) {
-            try {
-                classified.instance(i).setClassValue(classifier.classifyInstance(instances.instance(i)));
-            } catch (Exception e) {
-                LOG.error("\n", e);
-            }
-        }
-        // TRACE
-        if (LOG.isTraceEnabled())
-            LOG.trace("classified: \n" + classified);
-        // TRACE
-
-        Set<Entity> set = pp.instancesToEntities(classified);
-        LOG.info("classify done, size: " + set.size());
-        if (LOG.isDebugEnabled())
-            LOG.debug(classifier);
-        return set;
-    }
-
-    /**
-     * Reads files, init. instances and builds a classifier.
-     * 
-     * @param files
-     *            files to read as training data
-     */
-    public void training(String input, Map<String, Set<Entity>> toolResults, Map<String, String> oracle) throws Exception {
-        LOG.info("training ...");
-
-        // init. training data
-        IPostProcessing pp = new PostProcessing(new TokenManager(input), toolResults);
-        Map<String, String> labeledOracle = pp.getLabeledMap(oracle);
-        Map<String, Set<Entity>> labledToolResults = pp.getLabeledToolResults();
-
-        // DEBUG
-        if (LOG.isDebugEnabled()) {
-            LOG.debug("labeled entity:");
-
-            Set<Entity> set = new LinkedHashSet<>();
-            for (Entry<String, Set<Entity>> e : labledToolResults.entrySet())
-                set.addAll(e.getValue());
-
-            for (Entity e : set)
-                LOG.trace(e.getText());
-        }
-        // DEBUG
-
-        initInstances(pp.getLabeledInput(), labledToolResults, labeledOracle);
-        buildClassifier();
-    }
-
-    /**
-     * Evaluation
-     * 
-     * @throws Exception
-     */
-    public void eva() throws Exception {
-
-        if (isTrained) {
-            Evaluation eva = new Evaluation(instances);
-            eva.evaluateModel(classifier, instances);
-
-            // print summary
-            LOG.info("summary\n" + eva.toSummaryString());
-
-            // print the confusion matrix
-            StringBuffer cm = new StringBuffer();
-            double[][] cmMatrix = eva.confusionMatrix();
-            for (String cl : EntityClassMap.entityClasses) {
-                cm.append(cl + "\t");
-            }
-            cm.append("\n");
-            for (int i = 0; i < cmMatrix.length; i++) {
-                for (int ii = 0; ii < cmMatrix[i].length; ii++)
-                    cm.append(cmMatrix[i][ii] + "\t\t");
-                cm.append("\n");
-            }
-
-            LOG.info("confusion matrix\n" + cm.toString());
-
-            // measure
-            for (String cl : EntityClassMap.entityClasses) {
-                LOG.info("class: " + cl);
-                LOG.info("fMeasure: " + eva.fMeasure(EntityClassMap.entityClasses.indexOf(cl)));
-                LOG.info("precision: " + eva.precision(EntityClassMap.entityClasses.indexOf(cl)));
-                LOG.info("recall: " + eva.recall(EntityClassMap.entityClasses.indexOf(cl)));
-            }
-
-        } else {
-            LOG.error("Build/training a classifier first.");
-        }
-    }
-
-    public void setIsTrained(boolean bool) {
-        this.isTrained = bool;
-    }
-
-    public void setClassifier(Classifier classifier) {
-        this.classifier = classifier;
-    }
-
-    public Classifier getClassifier() {
-        return classifier;
-    }
+  public Classifier getClassifier() {
+    return classifier;
+  }
 }
