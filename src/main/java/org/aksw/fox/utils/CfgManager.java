c9ec699984d1e19c958b7b024778e570e8e15741
renespeck <speckrene@gmail.com>
2017-03-30 11:12:01 +0200

    Remove default never used cfg file.

--- src/main/java/org/aksw/fox/utils/CfgManager.java
+++ src/main/java/org/aksw/fox/utils/CfgManager.java
@@ -8,39 +8,37 @@ import org.apache.log4j.LogManager;
 import org.apache.log4j.Logger;
 
 public class CfgManager {
-    public final static String CFG_FOLDER = "data/fox/cfg";
-    public final static String CFG_FILE   = "app.xml";
+  public final static String CFG_FOLDER = "data/fox/cfg";
+  public static final Logger LOG = LogManager.getLogger(CfgManager.class);
 
-    public static final Logger LOG        = LogManager.getLogger(CfgManager.class);
+  /**
+   *
+   * @param className
+   * @return
+   */
+  public static XMLConfiguration getCfg(final String className) {
 
-    /**
-     * 
-     * @param className
-     * @return
-     */
-    public static XMLConfiguration getCfg(String className) {
+    final String file = CFG_FOLDER + File.separator + className + ".xml";
 
-        String file = CFG_FOLDER + File.separator + className + ".xml";
-        String fileDefault = CFG_FOLDER + File.separator + CFG_FILE;
+    if (FileUtil.fileExists(file)) {
 
-        if (FileUtil.fileExists(file))
-            fileDefault = file;
-
-        LOG.debug("load " + fileDefault);
-        try {
-            return new XMLConfiguration(fileDefault);
-        } catch (ConfigurationException e) {
-            LOG.error("\n Error while reading " + fileDefault, e);
-            return null;
-        }
+      try {
+        return new XMLConfiguration(file);
+      } catch (final ConfigurationException e) {
+        LOG.error("Error while reading " + file, e);
+      }
+    } else {
+      LOG.warn("Could not find " + file);
     }
+    return null;
+  }
 
-    /**
-     * 
-     * @param className
-     * @return
-     */
-    public static XMLConfiguration getCfg(Class<?> classs) {
-        return CfgManager.getCfg(classs.getName());
-    }
+  /**
+   *
+   * @param className
+   * @return
+   */
+  public static XMLConfiguration getCfg(final Class<?> classs) {
+    return CfgManager.getCfg(classs.getName());
+  }
 }
