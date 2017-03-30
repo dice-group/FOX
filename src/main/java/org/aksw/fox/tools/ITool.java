d4229746ec325f4551faf51be2b04f276d923e50
renespeck <speckrene@gmail.com>
2017-03-30 15:17:40 +0200

    Update interfaces.

---
+++ src/main/java/org/aksw/fox/tools/ITool.java
@@ -0,0 +1,20 @@
+package org.aksw.fox.tools;
+
+import java.util.concurrent.CountDownLatch;
+
+public interface ITool extends Runnable {
+
+  /**
+   * Returns the tools name.
+   *
+   * @return name
+   */
+  public String getToolName();
+
+  /**
+   * Sets a CountDownLatch object.
+   *
+   * @param cdl
+   */
+  public void setCountDownLatch(CountDownLatch cdl);
+}
--- src/main/java/org/aksw/fox/tools/ner/INER.java
+++ src/main/java/org/aksw/fox/tools/ner/INER.java
@@ -1,9 +1,9 @@
 package org.aksw.fox.tools.ner;
 
 import java.util.List;
-import java.util.concurrent.CountDownLatch;
 
 import org.aksw.fox.data.Entity;
+import org.aksw.fox.tools.ITool;
 
 /**
  * An interface for the use of a ner tool.
@@ -14,40 +14,26 @@ import org.aksw.fox.data.Entity;
  * @author rspeck
  *
  */
-public interface INER extends Runnable {
+public interface INER extends ITool {
 
   /**
    * Retrieves Entity objects from the give input String with sentences as plain text.
-   * 
+   *
    * @param input sentences as plain text
    * @return list entities
    */
   public List<Entity> retrieve(String input);
 
   /**
-   * Returns the tools name.
-   * 
-   * @return name
-   */
-  public String getToolName();
-
-  /**
-   * Sets a CountDownLatch object.
-   * 
-   * @param cdl
-   */
-  public void setCountDownLatch(CountDownLatch cdl);
-
-  /**
    * Sets the input with sentences as plain text.
-   * 
+   *
    * @param input
    */
   public void setInput(String input);
 
   /**
    * Returns results. Uses the {@link #retrieve(String input)} method to get the result list.
-   * 
+   *
    * @return results
    */
   public List<Entity> getResults();
