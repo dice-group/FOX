bed269c83822852521b6f02d2e3158929bc3e406
renespeck <speckrene@gmail.com>
2017-05-18 18:03:32 +0200

    Remove debug.

--- ScriptRun.sh
+++ ScriptRun.sh
@@ -1,6 +1,4 @@
 #!/bin/sh
 export MAVEN_OPTS="-Xmx16G -Dlog4j.configuration=file:data/fox/log4j.properties"
 
-# mvn clean compile -Dmaven.test.skip=falsejavadoc:javadoc && mvn exec:java  -Dexec.mainClass="org.aksw.fox.ui.FoxRESTful" -Dlog4j.configuration="file:log4j.properties"
-
-echo "debug run" && mvn compile -Dmaven.test.skip=true && mvn exec:java  -Dexec.mainClass="org.aksw.fox.ui.FoxRESTful" 
+nohup mvn clean compile -Dmaven.test.skip=false javadoc:javadoc && mvn exec:java  -Dexec.mainClass="org.aksw.fox.ui.FoxRESTful" > build.log &
