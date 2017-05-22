516f0aaf19ecf15e8556d0192677006c51d59675
renespeck <speckrene@gmail.com>
2017-05-22 10:16:11 +0200

    Update.

---
+++ src/main/java/org/aksw/fox/ui/FoxRESTful.java
@@ -0,0 +1,35 @@
+package org.aksw.fox.ui;
+
+import org.aksw.fox.data.exception.PortInUseException;
+import org.aksw.fox.utils.FoxCfg;
+import org.aksw.fox.webservice.FoxServer;
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
+
+/**
+ * Starts FOX web service.
+ *
+ * @author Ren&eacute; Speck <speck@informatik.uni-leipzig.de>
+ *
+ */
+public class FoxRESTful {
+  public static Logger LOG = LogManager.getLogger(FoxRESTful.class);
+
+  /**
+   * Starts FOX web service.
+   *
+   * @throws PortInUseException
+   */
+  public static void main(final String[] args) throws PortInUseException {
+    LOG.info("Fox web service starting ...");
+    final FoxServer server = new FoxServer();
+    if (FoxCfg.loadFile(FoxCfg.CFG_FILE)) {
+      try {
+        server.start();
+      } catch (final Exception e) {
+        LOG.error(e.getLocalizedMessage(), e);
+      }
+    }
+    LOG.info("Fox web service ready.");
+  }
+}
