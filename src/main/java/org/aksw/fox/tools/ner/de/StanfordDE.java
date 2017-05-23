1cf963f678ddcfddf96412443276bd8f69957f61
renespeck <speckrene@gmail.com>
2017-05-23 20:00:09 +0200

    Update.

--- src/main/java/org/aksw/fox/tools/ner/de/StanfordDE.java
+++ src/main/java/org/aksw/fox/tools/ner/de/StanfordDE.java
@@ -12,18 +12,25 @@ import org.aksw.fox.utils.FoxConst;
  *
  */
 public class StanfordDE extends StanfordCommon {
-  // https://github.com/stanfordnlp/CoreNLP/blob/master/src/edu/stanford/nlp/pipeline/StanfordCoreNLP-german.properties
 
+  /**
+   * <code>
+
+     https://github.com/stanfordnlp/CoreNLP/blob/master/src/edu/stanford/nlp/pipeline/StanfordCoreNLP-german.properties
+
+     </code>
+   */
   private static Properties props = new Properties();
 
   static {
     /* props.setProperty("annotators", "tokenize, ssplit, pos, lemma, ner, parse"); */
     props.setProperty("annotators", "tokenize, ssplit, pos, lemma, ner");
     props.setProperty("tokenize.language", "de");
-    props.setProperty("pos.model", "data/stanford/models/german-hgc.tagger");
-    props.setProperty("ner.model", "data/stanford/models/hgc_175m_600.crf.ser.gz");
+    props.setProperty("pos.model", "edu/stanford/nlp/models/pos-tagger/german/german-hgc.tagger");
+    props.setProperty("ner.model", "edu/stanford/nlp/models/ner/german.hgc_175m_600.crf.ser.gz");
     props.setProperty("ner.applyNumericClassifiers", "false");
     props.setProperty("ner.useSUTime", "false");
+
     /*
      * props.setProperty("parse.model", "edu/stanford/nlp/models/lexparser/germanFactored.ser.gz");
      */
@@ -37,6 +44,12 @@ public class StanfordDE extends StanfordCommon {
     entityClasses.put("I-PER", EntityClassMap.P);
     entityClasses.put("O", EntityClassMap.N);
     entityClasses.put("I-MISC", EntityClassMap.N);
+
+    entityClasses.put("B-ORG", EntityClassMap.O);
+    entityClasses.put("B-LOC", EntityClassMap.L);
+    entityClasses.put("B-PER", EntityClassMap.P);
+    entityClasses.put("O", EntityClassMap.N);
+    entityClasses.put("B-MISC", EntityClassMap.N);
   }
 
   public static void main(final String[] a) {
--- src/main/java/org/aksw/fox/tools/ner/es/StanfordES.java
+++ src/main/java/org/aksw/fox/tools/ner/es/StanfordES.java
@@ -21,8 +21,10 @@ public class StanfordES extends StanfordCommon {
      */
     props.setProperty("annotators", "tokenize, ssplit, pos, lemma, ner");
     props.setProperty("tokenize.language", "es");
-    props.setProperty("pos.model", "data/stanford/models/spanish-distsim.tagger");
-    props.setProperty("ner.model", "data/stanford/models/spanish.ancora.distsim.s512.crf.ser.gz");
+    props.setProperty("pos.model",
+        "edu/stanford/nlp/models/pos-tagger/spanish/spanish-distsim.tagger");
+    props.setProperty("ner.model",
+        "edu/stanford/nlp/models/ner/spanish.ancora.distsim.s512.crf.ser.gz");
     props.setProperty("ner.applyNumericClassifiers", "false");
     props.setProperty("ner.useSUTime", "false");
     // props.setProperty("parse.model", "edu/stanford/nlp/models/lexparser/spanishPCFG.ser.gz");
