8dbcfb232215cbc445810e34d1e391fbf2b8650a
renespeck <speckrene@gmail.com>
2017-06-16 15:07:14 +0200

    Update.

--- ScriptRun.sh
+++ ScriptRun.sh
@@ -1,4 +1,15 @@
 #!/bin/sh
 export MAVEN_OPTS="-Xmx16G -Dlog4j.configuration=file:data/fox/log4j.properties"
 
+cd spotlight
+
+./runDE.sh
+./runEN.sh
+./runEs.sh
+./runNL.sh
+./runFR.sh
+
+cd ..
+
+
 nohup mvn exec:java  -Dexec.mainClass="org.aksw.fox.ui.FoxRESTful" > logRun.log &
