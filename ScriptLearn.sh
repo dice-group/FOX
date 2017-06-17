27ea8e79c29fcc9f45169ab1173721e27a124af0
renespeck <speckrene@gmail.com>
2017-06-17 18:00:25 +0200

    Update.

--- ScriptLearn.sh
+++ ScriptLearn.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
 export MAVEN_OPTS="-Xmx16G -Dlog4j.configuration=file:data/fox/log4j.properties"
 
-nohup mvn exec:java -Dexec.mainClass="org.aksw.fox.ui.FoxCLI" -Dexec.args="-len -atrain -iinput/1" > learn.log &
+#ARGS="-len -atrain -iinput/4"
+ARGS="-len -atrain -iinput/Wikiner/aij-wikiner-en-wp3.bz2"
 
+
+nohup mvn exec:java -Dexec.mainClass="org.aksw.fox.ui.FoxCLI" -Dexec.args="$ARGS" > logLearn.log &
 
 #nohup java -Xmx8G -cp fox-${project.version}-jar-with-dependencies.jar org.aksw.fox.FoxCLI -lde -atrain -iinput/Wikiner/aij-wikiner-de-wp3.bz2 > learn.log &
