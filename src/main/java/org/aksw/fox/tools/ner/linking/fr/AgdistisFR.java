222bc3d00db1ee2e1731b995e309ee04a9849c7c
renespeck <speckrene@gmail.com>
2017-06-23 17:08:45 +0200

    Update agdistis wrappers.

---
+++ src/main/java/org/aksw/fox/tools/ner/linking/fr/AgdistisFR.java
@@ -0,0 +1,16 @@
+package org.aksw.fox.tools.ner.linking.fr;
+
+import org.aksw.fox.tools.ner.linking.common.Agdistis;
+import org.aksw.fox.utils.CfgManager;
+
+/**
+ * This class uses the Agdistis web service.
+ *
+ * @author Ren&eacute; Speck <speck@informatik.uni-leipzig.de>
+ *
+ */
+public class AgdistisFR extends Agdistis {
+  public AgdistisFR() {
+    super(CfgManager.getCfg(AgdistisFR.class));
+  }
+}
---
+++ src/main/java/org/aksw/fox/tools/ner/linking/nl/AgdistisNL.java
@@ -0,0 +1,16 @@
+package org.aksw.fox.tools.ner.linking.nl;
+
+import org.aksw.fox.tools.ner.linking.common.Agdistis;
+import org.aksw.fox.utils.CfgManager;
+
+/**
+ * This class uses the Agdistis web service.
+ *
+ * @author Ren&eacute; Speck <speck@informatik.uni-leipzig.de>
+ *
+ */
+public class AgdistisNL extends Agdistis {
+  public AgdistisNL() {
+    super(CfgManager.getCfg(AgdistisNL.class));
+  }
+}
